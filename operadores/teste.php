181a9c6a0daa2399e7da7b4dd8662a220de241fd
Lucas Silva <lucas.s-aguiar@hotmail.com>
2020-08-21 21:59:29 -0300

    Operadores com teste Web Server

---
+++ operadores/teste.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Hello World";
+
+
+$saldo = 34.5;//Operador de atribuição
+
+//aspas duplas aceitam string especial
+echo "Seu saldo é $saldo";
+
+//aspas simples apenas string pura
+echo '<br><br><br> Seu saldo é $saldo';
+
+$saldo = $saldo - 10;//Operador de subtração
+echo "<br><br><br> Seu saldo é $saldo";
+
+$saldo *=1000;//Operador de multiplicação
+
+echo "<br><br><br> Agora seu saldo é $saldo";   
+
